22cf411a42936589ac6f802b34707f81c801a35e
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-11 16:26:39 +0300

    pushing the whole app

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,30 +1,28 @@
-import { NextResponse } from "next/server";
-import { quizCreationSchema } from "@/schemas/forms/quiz";
-import { ZodError } from "zod";
 import { strict_output } from "@/lib/gpt";
 import { getAuthSession } from "@/lib/nextauth";
+import { getQuestionsSchema } from "@/schemas/questions";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
 
-export const POST = async (req: Request, res: Response) => {
+export async function POST(req: Request, res: Response) {
   try {
     const session = await getAuthSession();
-    if (!session?.user) {
-      return NextResponse.json(
-        {
-          error: "You must be logged in to create a quiz.",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
+    // if (!session?.user) {
+    //   return NextResponse.json(
+    //     { error: "You must be logged in to create a game." },
+    //     {
+    //       status: 401,
+    //     }
+    //   );
+    // }
     const body = await req.json();
-    const { amount, topic, type } = quizCreationSchema.parse(body);
+    const { amount, topic, type } = getQuestionsSchema.parse(body);
     let questions: any;
     if (type === "open_ended") {
       questions = await strict_output(
-        "You are an helpful AI that is able ot generate a set of questions and answers. The length of the answers must not exceed 15 words. Store all the sets of questions and answers in a JSON array.",
+        "You are a helpful AI that is able to generate a pair of question and answers, the length of each answer should not be more than 15 words, store all the pairs of answers and questions in a JSON array",
         new Array(amount).fill(
-          `You are to generate a random hard open-ended questions about "${topic}"`
+          `You are to generate a random hard open-ended questions about ${topic}`
         ),
         {
           question: "question",
@@ -33,22 +31,22 @@ export const POST = async (req: Request, res: Response) => {
       );
     } else if (type === "mcq") {
       questions = await strict_output(
-        "You are an helpful AI that is able ot generate a set of questions and answers. The length of the answers must not exceed 15 words. Store all the sets of questions and answers in a JSON array.",
+        "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words, store all answers and questions and options in a JSON array",
         new Array(amount).fill(
-          `You are to generate a random random mcq questions about "${topic}"`
+          `You are to generate a random hard mcq question about ${topic}`
         ),
         {
           question: "question",
           answer: "answer with max length of 15 words",
-          option1: "1st option with max length of 15 words",
-          option2: "2nd option with max length of 15 words",
-          option3: "3rd option with max length of 15 words",
+          option1: "option1 with max length of 15 words",
+          option2: "option2 with max length of 15 words",
+          option3: "option3 with max length of 15 words",
         }
       );
     }
     return NextResponse.json(
       {
-        questions,
+        questions: questions,
       },
       {
         status: 200,
@@ -57,13 +55,19 @@ export const POST = async (req: Request, res: Response) => {
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
-        {
-          error: error.issues,
-        },
+        { error: error.issues },
         {
           status: 400,
         }
       );
+    } else {
+      console.error("elle gpt error", error);
+      return NextResponse.json(
+        { error: "An unexpected error occurred." },
+        {
+          status: 500,
+        }
+      );
     }
   }
-};
+}
